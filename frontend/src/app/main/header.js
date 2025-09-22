4db655dac885d48c82ab585736e8730641d80b40
webdevopst5 <webdevopst5@gmail.com>
2025-09-22 18:52:59 +0530

    Header Design header and add flyout pop-up menu

--- frontend/src/app/main/header.js
+++ frontend/src/app/main/header.js
@@ -3,6 +3,7 @@
 import React, { useEffect } from 'react';
 import Link from 'next/link';
 import styles from './header.module.css';
+import Image from 'next/image';
 
 const Header = () => {
   useEffect(() => {
@@ -11,129 +12,175 @@ const Header = () => {
   }, []);
 
   return (
-    <header className={styles.headerArea}>
-    <div className={`container   ${styles.headerBlock} ${styles.h_container}`}>
-      <div className={`row ${styles.topbarBlock}`}>
-          <div className={`col-5 col-sm-6 col-lg-6`}>
-              <button className={styles.dashboardbtn}>Dashboard</button>
-          </div>
-          <div className={`col-7 col-sm-6 col-lg-6`}>
-            <div className='d-flex align-items-center justify-content-center'>
-            <div className={styles.buttonCustom}>
-                  <button>List Your Item</button>
-                </div>
-                <div className={styles.circlBtn}>
-                  <button aria-label="go">
-                    <svg fill="none" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                      <g clipRule="evenodd" fill="black" fillRule="evenodd">
-                        <path d="m4 16c0-.5523.44772-1 1-1h22c.5523 0 1 .4477 1 1s-.4477 1-1 1h-22c-.55228 0-1-.4477-1-1z" />
-                        <path d="m17.2929 6.29289c.3905-.39052 1.0237-.39052 1.4142 0l9 9.00001c.3905.3905.3905 1.0237 0 1.4142l-9 9c-.3905.3905-1.0237.3905-1.4142 0s-.3905-1.0237 0-1.4142l8.2929-8.2929-8.2929-8.29289c-.3905-.39053-.3905-1.02369 0-1.41422z" />
-                      </g>
-                    </svg>
-                  </button>
-                </div>
-          </div></div>
-      </div>
-      <div className={`row align-items-center`}>
+    <>
+      <script>
 
-        {/* Logo */}
-        <div className={`col-4 col-md-3 col-lg-2 ${styles.logoBlock} `}>
-          <Link href="/">
-            <img
-              className={styles.logo}
-              src="/images/global-imgs/site-logo.png"
-              alt="Logo"
-            />
-          </Link>
-        </div>
+      </script>
 
-        {/* Navbar */}
-        <div className={`col-8 col-md-9 d-flex d-md-flex justify-content-end justify-content-md-end d-lg-block justify-content-lg-center col-lg-6 order-1 order-lg-0 ${styles.col_lg_7}`}>
-          <nav className={`navbar navbar-expand-lg navbar-dark bg-transparent ${styles.navbar_n}`}>
-            <button
-              className={`navbar-toggler ${styles.navbar_toggler}`}
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-            >
-              <span className={`navbar-toggler-icon`}></span>
-            </button>
-
-            <div className={`collapse navbar-collapse justify-content-start  ${styles.navbarNav_n}`} id="navbarNav">
-              <ul className={`navbar-nav w-100 align-items-center justify-content-around  ${styles.roh_navbarNav}`}>
-                <li className="nav-item">
-                  <Link className={styles.navLink} href="/">Home</Link>
-                </li>
-                <li className={`nav-item ${styles.dropdown}`}>
-                  <a
-                    className={`nav-link dropdown_toggle ${styles.dropdown_toggle} ${styles.navLink}`}
-                    href="/products/"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Services 
-                  </a>
-                  <div className={`dropdown-menu ${styles.dropdown_menu}`} aria-labelledby="navbarDropdown">
-                    <Link className={`dropdown-item ${styles.dropdown_item}`} href="#">Vehicles</Link>
-                    <Link className={`dropdown-item ${styles.dropdown_item}`} href="#">Electronics</Link>
-                    <Link className={`dropdown-item ${styles.dropdown_item}`} href="#">Home Appliances</Link>
-                    <Link className={`dropdown-item ${styles.dropdown_item}`} href="#">Tools & Equipment</Link>
-                    <Link className={`dropdown-item ${styles.dropdown_item}`} href="#">Events & Party</Link>
-                    <Link className={`dropdown-item ${styles.dropdown_item}`} href="#">Baby & Kids</Link>
-                    <Link className={`dropdown-item ${styles.dropdown_item}`} href="#">Travel & Camping</Link>
-                    <Link className={`dropdown-item ${styles.dropdown_item}`} href="#">Studio & Production</Link>
-                    <Link className={`dropdown-item ${styles.dropdown_item}`} href="#">Office & Furniture</Link>
-                    <Link className={`dropdown-item ${styles.dropdown_item}`} href="#">Miscellaneous</Link>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <a className={styles.navLink} href="#">How It Works</a>
-                </li>
-                <li className="nav-item">
-                  <Link className={styles.navLink} href="#">FAQs</Link>
-                </li>
-                <li className="nav-item">
-                  <a className={styles.navLink} href="#">About Us</a>
-                </li>
-                <li className="nav-item">
-                  <a className={styles.navLink} href="#">Contact Us</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </div>
-
-        {/* CTA + Dashboard */}
-        <div className={`col-6 col-md-8 col-lg-4 d-none d-md-none d-lg-flex  justify-content-end ${styles.col_lg_2}`}>
-          <div className={`d-flex align-items-center ${styles.topBtns}`}>
-            <Link href="/dashboard" className={styles.loginBtn}>
-              Dashboard
-            </Link>
-
-            <Link href="/become-a-host" style={{ textDecoration: 'none' }}>
-              <div className="d-flex align-items-center">
-                <div className={styles.buttonCustom}>
-                  <button>List Your Item</button>
-                </div>
-                <div className={styles.circlBtn}>
-                  <button aria-label="go">
-                    <svg fill="none" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                      <g clipRule="evenodd" fill="black" fillRule="evenodd">
-                        <path d="m4 16c0-.5523.44772-1 1-1h22c.5523 0 1 .4477 1 1s-.4477 1-1 1h-22c-.55228 0-1-.4477-1-1z" />
-                        <path d="m17.2929 6.29289c.3905-.39052 1.0237-.39052 1.4142 0l9 9.00001c.3905.3905.3905 1.0237 0 1.4142l-9 9c-.3905.3905-1.0237.3905-1.4142 0s-.3905-1.0237 0-1.4142l8.2929-8.2929-8.2929-8.29289c-.3905-.39053-.3905-1.02369 0-1.41422z" />
-                      </g>
-                    </svg>
-                  </button>
-                </div>
+      <header className={`mainbar  ${styles.roh_header_area} ${styles.roh_is_sticky}`}>
+        <div className={`container py-2 ${styles.roh_header_block}`}>
+          <div className={`header_manage`}>
+            <div className={`row align-items-center d-flex justify-content-between`}>
+              <div className={`col-4 col-md-6 col-lg-2 ${styles.roh_logo_block}`}>
+                <Link href="/">
+                  <Image className={styles.roh_logo} src="/images/global-imgs/site-logo.png" alt="Logo" width={100} height={36} />
+                </Link>
               </div>
-            </Link>
+              <div className={`col-2 col-md-2 col-lg-7 order-1 order-lg-0 ${styles.roh_desktop_headerNav}`}>
+                <nav className={`navbar navbar-expand-lg navbar-dark bg-transparent ${styles.roh_header_nav}`}>
+                 <div className={`navbar-collapse justify-content-start collapse show ${styles.roh_navbarNav}`} id="navbarNav">
+                    <ul className={`navbar-nav w-100 justify-content-around ${styles.roh_navBar}`}>
+                      <li className={`nav-item `}>
+                        <a className={`nav-link ${styles.roh_navLink}`} href="/">Home</a>
+                      </li>
+                      <li className={`nav-item dropdown ${styles.roh_dropdown}`}>
+                        <a
+                          className={`nav-link dropdown_toggle ${styles.dropdown_toggle} ${styles.roh_navLink}`} href="/products/" id="navbarDropdown"> Services
+                        </a>
+                        <div className={`dropdown-menu ${styles.roh_dropdown_menu}`} aria-labelledby="navbarDropdown">
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Vehicles</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Electronics</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Home Appliances</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Tools & Equipment</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Events & Party</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Baby & Kids</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Travel & Camping</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Studio & Production</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Office & Furniture</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Miscellaneous</Link>
+                        </div>
+                      </li>
+
+                      <li className={`nav-item`}>
+                        <a className={`nav-link ${styles.roh_navLink}`} href="#">How It Works</a>
+                      </li>
+                      <li className={`nav-item`}>
+                        <a className={`nav-link ${styles.roh_navLink}`} href="/faq.html">FAQs</a>
+                      </li>
+                      <li className={`nav-item`}>
+                        <a className={`nav-link ${styles.roh_navLink}`} href="#">About Us</a>
+                      </li>
+                      <li className={`nav-item`}>
+                        <a className={`nav-link ${styles.roh_navLink}`} href="#">Contact Us</a>
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+              </div>
+              <div className={`col-6 col-md-4 col-lg-3 text-end`}>
+                <div className={`d-flex align-items-center ${styles.roh_headerRight_button}`}>
+                  <Link href="/become-a-host" style={{ textDecoration: 'none' }}>
+                    <div className={`d-flex align-items-center justify-content-end ${styles.roh_top_btns}`}>
+                      <div className={`${styles.roh_button_custom}`}>
+                        <button>List Your Item</button>
+                      </div>
+                      <div className={`${styles.roh_circl_btn}`}>
+                        <button>
+                          <Image src="/arrow.svg" alt="Arrow Right" width={30} height={30} />
+                        </button>
+                      </div>
+                    </div>
+                  </Link>
+
+                  <div className={`roh_dashboard_profile`}>
+                    <div className={`${styles.roh_profile_box}`}>
+                      <div className={`dropdown`}>
+                      <button type="button" data-bs-toggle="dropdown" aria-expanded="false"><Image src="/user.svg" alt="User" width={30} height={30} /></button>
+
+  <div className={`dropdown-menu profil-dropdown p-0`}>
+  <div className={`roh_adrop_outer`} style={{ display: 'block'}}>
+  <div className={`${styles.roh_adrop_box_wrap}`}>
+    <ul className={`${styles.roh_adrop_list}`}>
+      <li>
+        <a href="/dashboard" className={styles.loginBtn}>
+        <Image src="/user1.svg" alt="Profile" width={20} height={20} /> Profile </a>
+      </li>
+      <li>
+        <a href="#">
+        <Image src="/support.svg" alt="Support" width={20} height={20} /> Support </a>
+      </li>
+      <li>
+        <a href="#">
+        <Image src="chat.svg" alt="Inbox" width={20} height={20} /> Inbox </a>
+      </li>
+      <li>
+        <a href="#">
+        <Image src="/setting-lines.svg" alt="Setting" width={20} height={20} /> Settings </a>
+      </li>
+      <li>
+        <a href="#">
+        <Image src="/log-in.svg" alt="Log-In" width={20} height={20} /> Login </a>
+      </li>
+      <li>
+        <a href="#">
+        <Image src="/logout.svg" alt="Log-Out" width={20} height={20} /> Logout </a>
+      </li>
+    </ul>
+  </div>
+</div>
+  </div>
+</div>
+                    </div>
+                  </div>
+                  
+                   {/*------------------- Mobile menu Pop Up Start-----------------------*/}
+                   <div className={`${styles.roh_mobileMenu}`}>
+                    <button type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
+                      <Image src="/menu-icon.svg" alt="Mobile Menu" width={30} height={30} />
+                    </button>
+                    <div className={`offcanvas offcanvas-start`} data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+                      <div className={`offcanvas-header ${styles.roh_offcanvas_header}`}>
+                        <Image className={styles.roh_logo} src="/images/global-imgs/site-logo.png" alt="Logo" width={100} height={36} />
+                        <button type="button" class={`btn-close`} data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                      </div>
+                      <div className={`offcanvas-body ${styles.roh_offcanvas_body}`}>
+                      <ul className={`navbar-nav w-100 justify-content-around ${styles.roh_navBar}`}>
+                      <li className={`nav-item `}>
+                        <a className={`nav-link ${styles.roh_navLink}`} href="/">Home</a>
+                      </li>
+                      <li className={`nav-item dropdown ${styles.roh_dropdown}`}>
+                        <a
+                          className={`nav-link dropdown_toggle ${styles.dropdown_toggle} ${styles.roh_navLink}`} href="/products/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" > Services
+                        </a>
+                        <div className={`dropdown-menu ${styles.roh_dropdown_menu}`} aria-labelledby="navbarDropdown">
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Vehicles</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Electronics</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Home Appliances</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Tools & Equipment</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Events & Party</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Baby & Kids</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Travel & Camping</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Studio & Production</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Office & Furniture</Link>
+                          <Link className={`dropdown-item ${styles.roh_dropdown_item}`} href="#">Miscellaneous</Link>
+                        </div>
+                      </li>
+                      <li className={`nav-item`}>
+                        <a className={`nav-link ${styles.roh_navLink}`} href="#">How It Works</a>
+                      </li>
+                      <li className={`nav-item`}>
+                        <a className={`nav-link ${styles.roh_navLink}`} href="/faq.html">FAQs</a>
+                      </li>
+                      <li className={`nav-item`}>
+                        <a className={`nav-link ${styles.roh_navLink}`} href="#">About Us</a>
+                      </li>
+                      <li className={`nav-item`}>
+                        <a className={`nav-link ${styles.roh_navLink}`} href="#">Contact Us</a>
+                      </li>
+                    </ul>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/*------------------- Mobile menu Pop Up END-----------------------*/}
+                </div>
+
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </header>
+      </header>
+    </>
   );
 };
 
