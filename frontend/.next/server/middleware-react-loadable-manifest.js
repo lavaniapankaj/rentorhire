371f6c672e476889a344b8ada4a0e9574b4a8b70
Vishnu Kumawat <ecmascript.php@gmail.com>
2025-09-27 14:24:34 +0530

    Remove .next folder from repo

--- frontend/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app/main/header.js -> bootstrap/dist/js/bootstrap.bundle.min.js\":{\"id\":\"app/main/header.js -> bootstrap/dist/js/bootstrap.bundle.min.js\",\"files\":[\"static/chunks/_app-pages-browser_node_modules_bootstrap_dist_js_bootstrap_bundle_min_js.js\"]}}"
